ae14ab67ca675db25bc9796f709c13cb5589a638
jhansimani <jhansimanim@gmail.com>
2022-11-25 17:37:19 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, VERSION } from '@angular/core';
-import { filter, map } from 'rxjs';
+import { Component, OnDestroy, OnInit, VERSION } from '@angular/core';
+import { filter, map, of, switchMap } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 import { CustomService } from './custom.service';
 
 @Component({
@@ -7,9 +8,11 @@ import { CustomService } from './custom.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   name = 'Angular ' + VERSION.major;
   transformedNumArray: any = [];
+  ofObse = of(1, 2, 3);
+  ofObseSubscription: any;
   constructor(private service: CustomService) {}
   ngOnInit() {
     this.service.numArray
@@ -24,5 +27,30 @@ export class AppComponent implements OnInit {
         this.transformedNumArray = data;
         console.log('hhh', this.transformedNumArray);
       });
+    // this.ofObseSubscription = this.ofObse
+    //   .pipe(
+    //     switchMap((val) => {
+    //       console.log('Source value ' + val);
+    //       console.log('starting new observable');
+    //       return of('a', 'b', 'c');
+    //     })
+    //   )
+    //   .subscribe((val) => {
+    //     console.log(val);
+    //   });
+    this.ofObseSubscription = this.ofObse
+      .pipe(
+        
+        mergeMap((val) => {
+          console.log('Source value ' + val);
+          console.log('starting new observable');
+          return of('a', 'b', 'c');
+        })
+      )
+      .subscribe((val) => {
+        console.log(val);
+      });
   }
+
+  ngOnDestroy() {}
 }
